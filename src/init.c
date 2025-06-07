970125a67ac28c4b2b59185e580b8b37c9a80b4c
cosimini <davide.scalcon@studenti.unipd.it>
2025-06-07 17:16:24 +0000

    Implement WAITBIT macro

--- src/init.c
+++ src/init.c
@@ -1,14 +1,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Utils
+#define MASK(N) ((0b1 << N) - 1)
 #define SET_BIT(reg, bit, val) SET_BIT##val(((uint32_t*) reg), bit)
 #define SET_BIT0(reg, idx) (*reg &= ~(1 << idx))
 #define SET_BIT1(reg, idx) (*reg |= (1 << idx))
-
-#define MASK(N) ((0b1 << N) - 1)
 #define SET_MSK(REG, IDX, VAL, SIZE) *(uint32_t*) REG = ( (*(uint32_t*) REG) & ~(MASK(SIZE) << IDX) ) | ((VAL & MASK(SIZE)) << IDX)
-// TODO #define WAIT_BIT(REG, IDX) do {} while( ((*(uint32_t*) REG) & ((uint32_t) 1 << IDX)) == 0)
+#define WAITBIT(REG, IDX) while( ((*(uint32_t*) REG) & ((uint32_t) 1 << IDX)) == 0 )
 
+// Registers definitions
 #define RCC_OFFSET 0x40023800
 #define RCC_CR      (RCC_OFFSET + 0x00)
 #define RCC_PLLCFGR (RCC_OFFSET + 0x04)
@@ -31,33 +32,39 @@
 #define GPIO_ODR 0x14
 
 void init(void) {
+
   /*
-   * CLOCK, FLASH and POWER
+   * FLASH
    */
-  SET_MSK(FLASH_ACR, 0, 3, 4); // Set the flash memory laetncy in control register FLASH_ACR in order to match the flash speed with the clock speed
-  SET_MSK(PWR_CR, 14, 0b11, 2); // Set PWR_CR to 11 to enable clock <= 100MHz
+  SET_MSK(FLASH_ACR,    0,  3,    4); // Set the flash memory latency to 3, as the clock will be 976MHz
 
-  SET_BIT(RCC_CR, 16, 1); // HSEON : Enable High Speed External XTAL
-  SET_BIT(RCC_CR, 19, 1); // CSSON : Enable Clock Security System
-  while(((*(uint32_t*) RCC_CR) & ((uint32_t) 0b1 << 17)) == 0); // Wait for HSE to be ready
+  /*
+   * POWER
+   */
+  SET_MSK(PWR_CR,       14, 0b11, 2); // Set VOS bits to 11, to enable clock <= 100MHz
 
-  SET_BIT(RCC_PLLCFGR, 22, 1); // PLLSRC : Set the source for PLL to HSE
-  SET_MSK(RCC_CFGR, 16, 25, 5); // Set HSE prescaler of RTC so that RTC is 1MHz
-  SET_MSK(RCC_PLLCFGR, 0, 25, 6);  // Set PLL M (input divider to 25)
-  SET_MSK(RCC_PLLCFGR, 6, 192, 9); // Set N to 192
-  SET_MSK(RCC_PLLCFGR, 16, 0, 2); // Set P to 2 - SYSCLK is 96MHz
-  SET_MSK(RCC_PLLCFGR, 24, 4, 4);  // Set Q to 4
-  SET_BIT(RCC_CR, 24, 1); // PLLON : Enable the PLL
-  while( ((*(uint32_t*) RCC_CR) & ((uint32_t) 1 << 25)) == 0); // Wait for PLL to be ready
-  SET_BIT(RCC_CFGR,  0, 0); // 0. Use the PLL output as SYSCLK
-  SET_BIT(RCC_CFGR,  1, 1); // 1. Use the PLL output as SYSCLK
-
-  SET_MSK(RCC_CFGR, 13, 0b110, 3); // Set APB2 clock to 12MHz (96MHz / 8)
-  SET_BIT(RCC_AHB1ENR,  2, 1); // Enable port C clock (builtin LED is connected on C13)
-  SET_BIT(RCC_AHB1ENR,  0, 1); // Enable port A clock (USART1 uses this port)
-  SET_BIT(RCC_AHB1ENR,  7, 1); // Enable USB's 48MHz clock
-  SET_BIT(RCC_AHB1ENR, 22, 1); // Enable DMA2 clock (on channel 4, USART1 is connected, stream 2, 5 and 7)
-  SET_BIT(RCC_APB2ENR,  4 ,1); // Enable USART1 clock
+  /*
+   * CLOCK
+   */
+  SET_BIT(RCC_CR,       16, 1);        // HSEON : Enable High Speed External XTAL
+  SET_BIT(RCC_CR,       19, 1);        // CSSON : Enable Clock Security System
+  WAITBIT(RCC_CR,       17);           // Wait for HSE to be ready
+  SET_BIT(RCC_PLLCFGR,  22, 1);        // PLLSRC : Set the source for PLL to HSE
+  SET_MSK(RCC_CFGR,     16, 25,   5);  // Set HSE prescaler of RTC so that RTC is 1MHz
+  SET_MSK(RCC_PLLCFGR,  0,  25,   6);  // Set PLL M (input divider to 25)
+  SET_MSK(RCC_PLLCFGR,  6,  192,  9);  // Set N to 192
+  SET_MSK(RCC_PLLCFGR,  16, 0,    2);  // Set P to 2 - SYSCLK is 96MHz
+  SET_MSK(RCC_PLLCFGR,  24, 4,    4);  // Set Q to 4
+  SET_BIT(RCC_CR,       24, 1);        // PLLON : Enable the PLL
+  WAITBIT(RCC_CR,       25);           // Wait for PLL to be ready
+  SET_BIT(RCC_CFGR,     0,  0);        // 0. Use the PLL output as SYSCLK
+  SET_BIT(RCC_CFGR,     1,  1);        // 1. Use the PLL output as SYSCLK
+  SET_MSK(RCC_CFGR,     13, 6,    3);  // Set APB2 clock to 12MHz (96MHz / 8)
+  SET_BIT(RCC_AHB1ENR,  2,  1);        // Enable port C clock (builtin LED is connected on C13)
+  SET_BIT(RCC_AHB1ENR,  0,  1);        // Enable port A clock (USART1 uses this port)
+  SET_BIT(RCC_AHB1ENR,  7,  1);        // Enable USB's 48MHz clock
+  SET_BIT(RCC_AHB1ENR,  22, 1);        // Enable DMA2 clock (on channel 4, USART1 is connected, stream 2, 5 and 7)
+  SET_BIT(RCC_APB2ENR,  4,  1);        // Enable USART1 clock
 
 
   /*
